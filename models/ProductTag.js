9fe74ea5d128196d30614ef68b5113c5b4d10e8c
Violet Stanziani <stanzianiviolet@gmail.com>
2024-07-09 21:22:49 -0400

    added to Tag and ProductTag models

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -6,7 +6,20 @@ class ProductTag extends Model {}
 
 ProductTag.init(
   {
-    // define columns
+    id: {
+      type: DataType.INTEGER,
+      allowNull: false, 
+      primaryKey: true, 
+      autoIncrement: true,
+    }, 
+    product_id: {
+      type: DataType.INTEGER,
+
+    }, 
+    tag_id: {
+      type: DataType.INTEGER, 
+    }
+
   },
   {
     sequelize,
