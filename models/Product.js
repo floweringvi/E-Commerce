7d248678a663377a854f8de218747c835f5630dc
Violet Stanziani <stanzianiviolet@gmail.com>
2024-07-09 22:46:11 -0400

    corrected DataType typo to DataTypes

--- models/Product.js
+++ models/Product.js
@@ -10,24 +10,24 @@ class Product extends Model {}
 Product.init(
   {
     id:{
-      type: DataType.INTEGER,
+      type: DataTypes.INTEGER,
       allowNull: false,
       primaryKey: true, 
       autoIncrement: true,
     },
     product_name: {
-      type: DataType.STRING,
+      type: DataTypes.STRING,
       allowNull: false,
     },
     price: {
-      type: DataType.DECIMAL,
+      type: DataTypes.DECIMAL,
       allowNull: false,
       validate: {
         isDecimal: true, 
       }
     },
     stock: {
-      type: DataType.INTEGER,
+      type: DataTypes.INTEGER,
       allowNull:false, 
       defaultValue: 10, 
       validate: {
@@ -36,7 +36,7 @@ Product.init(
 
     }, 
     category_id: {
-      type: DataType.INTEGER,
+      type: DataTypes.INTEGER,
       references: {
         model: "category", 
         key: "id",
--- models/ProductTag.js
+++ models/ProductTag.js
@@ -7,13 +7,13 @@ class ProductTag extends Model {}
 ProductTag.init(
   {
     id: {
-      type: DataType.INTEGER,
+      type: DataTypes.INTEGER,
       allowNull: false, 
       primaryKey: true, 
       autoIncrement: true,
     }, 
     product_id: {
-      type: DataType.INTEGER,
+      type: DataTypes.INTEGER,
       referencs: {
         model: 'product', 
         key: 'id',
@@ -21,7 +21,7 @@ ProductTag.init(
 
     }, 
     tag_id: {
-      type: DataType.INTEGER, 
+      type: DataTypes.INTEGER, 
       references: {
         model: 'tag', 
         key:'id',
