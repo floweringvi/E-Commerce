0fc7a5385882b5a0fe0c3c1b6b2212380bae3b6d
Violet Stanziani <stanzianiviolet@gmail.com>
2024-07-09 21:54:40 -0400

    category routes get, post, deleted and get byid finished

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,17 +3,45 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
+  try{ 
+    const categoryData = await Category.findAll({
+      include: [{model: Product}]
+    });
+    res.status(200).json(categoryData);
+
+  } catch(err){
+    res.status(500).json(err);
+  }
   // find all categories
   // be sure to include its associated Products
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
+  try {
+    const categoryData = await Category.findByPK(req.params.id, {
+      include: [{ model: Product}]
+    }); 
+
+    if(!categoryData){
+      res.status(404).json.apply({message: "No such category exists!"});
+      return;
+    }
+    res.status(200).json(categoryData);
+  }catch(err){
+    res.status(500).json(err)
+  }
   // find one category by its `id` value
   // be sure to include its associated Products
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
+  try{
+    const categoryData = await Category.create(req.body);
+    res.status(200).json(categoryData);
+  } catch(err) {
+    res.status(500).json(err);
+  }
   // create a new category
 });
 
@@ -21,7 +49,24 @@ router.put('/:id', (req, res) => {
   // update a category by its `id` value
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
+  try{
+    const categoryData = await Category.destroy({
+      where:{
+        id: req.params.id
+      },
+    });
+
+    if(!categoryData){
+      res.status(404).json({message: "No such category exists"});
+      return;
+    }
+
+    res.status(200).json(categoryData);
+  } catch(err) {
+    res.status(500).json(err);
+  }
+
   // delete a category by its `id` value
 });
 
