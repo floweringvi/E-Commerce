0d57056d576c1ff8a08ea5d9ab99cd4df21ab4b0
Violet Stanziani <stanzianiviolet@gmail.com>
2024-07-09 21:18:42 -0400

    added to category model and product model

--- models/Product.js
+++ models/Product.js
@@ -9,7 +9,30 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
-    // define columns
+    id:{
+      type: DataType.INTEGER,
+      allowNull: false,
+      primaryKey: true, 
+      autoIncrement: true,
+    },
+    product_name: {
+      type: DataType.STRING,
+      allowNull: false,
+    },
+    price: {
+      type: DataType.DECIMAL,
+      allowNull: false,
+    },
+    stock: {
+      type: DataType.INTEGER,
+      allowNull:false, 
+      defaultValue: 10, 
+
+    }, 
+    category_id: {
+      type: DataType.INTEGER,
+      
+    }
   },
   {
     sequelize,
